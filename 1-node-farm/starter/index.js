75d961d547b027dafea99bb763ca38b750d33735
anxhirr <anxhirr@gmail.com>
2023-07-03 22:54:03 +0200

    routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,25 @@
 const http = require('http')
+const url = require('url')
 
 ///////////////////
 // SERVER
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from my server')
+  console.log(req.url)
+
+  const pathName = req.url
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('this is overview')
+  } else if (pathName === '/product') {
+    res.end('this is product')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-header': 'hello-word',
+    })
+    res.end('Page not found')
+  }
 })
 
 server.listen(8000, '127.0.0.1', () => {

d24098f4d5c516a49a66a5002895f50c189daa09
anxhirr <anxhirr@gmail.com>
2023-07-03 23:09:10 +0200

    simple api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,12 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 ///////////////////
 // SERVER
+
+const devData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const devDataObj = JSON.parse(devData)
 
 const server = http.createServer((req, res) => {
   console.log(req.url)
@@ -13,6 +17,9 @@ const server = http.createServer((req, res) => {
     res.end('this is overview')
   } else if (pathName === '/product') {
     res.end('this is product')
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(devData)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
